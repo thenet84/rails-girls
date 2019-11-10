3b2118803fb17924b3b193dceaa86385d2c0eff2
Denet <the.net.wo@gmail.com>
2019-11-10 23:55:41 +0000

    Add comments and some styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-    belongs_to: idea
+    belongs_to :idea
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,4 @@
 class Idea < ApplicationRecord
-    has_many: comments
+    has_many :comments
     mount_uploader :picture, PictureUploader
 end
